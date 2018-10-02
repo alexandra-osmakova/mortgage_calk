02c4a07f152f8201a9ab87401d4d576781b1b800
Александра Осмакова <aleksandra.osmakova@MacBook-Pro-Aleksandra.local>
2018-10-02 19:10:48 +0300

    mobile version added email sending function adaptation for middle screen some adaptation for big screen

---
+++ mail.php
@@ -0,0 +1,23 @@
+<?php
+    header("Location: index.html?sended=true");
+    $user_name = $_POST['name'];
+    $email = $_POST['email'];
+    $telephone = $_POST['phone-number'];
+
+    $email_from = 'lolololo@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $user_name.\n".
+                    "User Telephon Number: $telephone.\n".
+                    "User Email: $email.\n".
+
+
+
+    
+    $to = "579531535@etlgr.com, 206315401@etlgr.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $ok = mail($to, $email_subject, $email_body, $headers);
+?>
